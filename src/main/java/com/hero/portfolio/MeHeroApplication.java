b2873a10c66dd60573527f72709e4a865b733b18
harshal342 <harshalwagh342@gmail.com>
2025-09-08 11:09:19 +0530

    new Portfolio

---
+++ src/main/java/com/hero/portfolio/MeHeroApplication.java
@@ -0,0 +1,13 @@
+package com.hero.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeHeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hero/portfolio/MeHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hero.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
